5d6d55aa0c5808cd376b93ccadd52f8b1c9847a4
seppe0327 <seppe0327@users.noreply.github.com>
2025-01-27 06:18:04 +0000

    Auto deploy by Github Actions, 2025/01/27 06:18:04

---
+++ files/index.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const app = express();
+const { exec, execSync } = require('child_process');
+const port = process.env.SERVER_PORT || process.env.PORT || 3000;        
+const UUID = process.env.UUID || '59c63d49-88da-4236-b1d8-cd409e4b1e9d'; //
+const NEZHA_SERVER = process.env.NEZHA_SERVER || '';     
+const NEZHA_PORT = process.env.NEZHA_PORT || '';                     //
+const NEZHA_KEY = process.env.NEZHA_KEY || '';   //
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';     //
+const ARGO_AUTH = process.env.ARGO_AUTH || '';
+const CFIP = process.env.CFIP || 'www.visa.com.tw';
+const NAME = process.env.NAME || 'Choreo-US';
+
+// root route
+app.get("/", function(req, res) {
+  res.send("Hello world!");
+});
+
+const metaInfo = execSync(
+  'curl -s https://speed.cloudflare.com/meta | awk -F\\" \'{print $26"-"$18}\' | sed -e \'s/ /_/g\'',
+  { encoding: 'utf-8' }
+);
+const ISP = metaInfo.trim();
+
+// sub subscription
+app.get('/sub', (req, res) => {
+  const VMESS = { v: '2', ps: `${NAME}-${ISP}`, add: CFIP, port: '443', id: UUID, aid: '0', scy: 'none', net: 'ws', type: 'none', host: ARGO_DOMAIN, path: '/vmess?ed=2048', tls: 'tls', sni: ARGO_DOMAIN, alpn: '' };
+  const vlessURL = `vless://${UUID}@${CFIP}:443?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Fvless?ed=2048#${NAME}-${ISP}`;
+  const vmessURL = `vmess://${Buffer.from(JSON.stringify(VMESS)).toString('base64')}`;
+  const trojanURL = `trojan://${UUID}@${CFIP}:443?security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Ftrojan?ed=2048#${NAME}-${ISP}`;
+  
+  const base64Content = Buffer.from(`${vlessURL}\n\n${vmessURL}\n\n${trojanURL}`).toString('base64');
+
+  res.type('text/plain; charset=utf-8').send(base64Content);
+});
+
+// run-nezha
+  let NEZHA_TLS = '';
+  if (NEZHA_SERVER && NEZHA_PORT && NEZHA_KEY) {
+    const tlsPorts = ['443', '8443', '2096', '2087', '2083', '2053'];
+    if (tlsPorts.includes(NEZHA_PORT)) {
+      NEZHA_TLS = '--tls';
+    } else {
+      NEZHA_TLS = '';
+    }
+  const command = `nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &`;
+  try {
+    exec(command);
+    console.log('swith is running');
+
+    setTimeout(() => {
+      runWeb();
+    }, 2000);
+  } catch (error) {
+    console.error(`swith running error: ${error}`);
+  }
+} else {
+  console.log('NEZHA variable is empty, skip running');
+  runWeb();
+}
+
+// run-xr-ay
+function runWeb() {
+  const command1 = `nohup ./web -c ./config.json >/dev/null 2>&1 &`;
+  exec(command1, (error) => {
+    if (error) {
+      console.error(`web running error: ${error}`);
+    } else {
+      console.log('web is running');
+
+      setTimeout(() => {
+        runServer();
+      }, 2000);
+    }
+  });
+}
+
+// run-server
+function runServer() {
+  let command2 = '';
+  if (ARGO_AUTH.match(/^[A-Z0-9a-z=]{120,250}$/)) {
+    command2 = `nohup ./server tunnel --region us --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH} >/dev/null 2>&1 &`;
+  } else {
+    command2 = `nohup ./server tunnel --region us --edge-ip-version auto --config tunnel.yml run >/dev/null 2>&1 &`;
+  }
+
+  // 使用 exec 直接运行命令并将输出打印到控制台
+  exec(command2, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`server running error: ${error}`);
+      return;
+    }
+    if (stdout) {
+      console.log(stdout);
+    }
+    if (stderr) {
+      console.error(stderr);
+    }
+  });
+
+  console.log('server is running');
+}
+
+runServer();
+
+app.listen(port, () => console.log(`App is listening on port ${port}!`));
